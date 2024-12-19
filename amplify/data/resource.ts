00e17ce1c36f9e66233d112e466d1b423d5ddda6
akash-nv <akashdesai@medianv.com>
2024-12-19 12:18:07 +0530

    define authorization for todo and note

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,12 +10,12 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
-    }),
+    }).authorization((allow) => [allow.publicApiKey()]),
     Note: a
     .model({
       content: a.string(),
       complete: a.boolean()
-    }),
+    }).authorization((allow) => [allow.publicApiKey()]),
     Clinic: a
     .customType({
       id: a.id(),
